098431e10f61e1f5261124a69d2104d620afca36
LEONARDO HENRIQUE ROMANINI <987308346@itaud.des.ihf>
2020-06-21 09:46:27 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/itau/curso/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.itau.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.itau.curso.entities.Order;
+import com.itau.curso.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}

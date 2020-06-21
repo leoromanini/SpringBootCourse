e38c5acf95d2814b1ad3be522df7326eba6a946b
LEONARDO HENRIQUE ROMANINI <987308346@itaud.des.ihf>
2020-06-21 12:04:13 -0300

    Entity Product e Category

---
+++ src/main/java/com/itau/curso/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.itau.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.itau.curso.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
